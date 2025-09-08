982374efac27b7bceea390aa9a0b88217214c8f8
rudraa19 <rudra19206@gmail.com>
2025-09-08 06:17:36 +0530

    chore: add delete docs functionality

--- client/src/components/DocCard.jsx
+++ client/src/components/DocCard.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
+import { client } from "../../config";
+import { useToken } from "@/utils/useToken";
 
 const DocCared = ({ id = "123abc", name = "Untitled" }) => {
+    const token = useToken();
+    async function handleDelete(id) {
+        try {
+            await client.delete(`/v1/docs/${id}`, {
+                headers: { token }
+            });
+            alert("Doc deleted successfully!");
+        } catch (err) {
+            alert("Error occured during deleting doc!");
+            console.log(err);
+        }
+    }
     return (
         <>
             <Link to={id}>
                 <Card className="max-w-[800px] m-1 p-3 flex-row items-center justify-between">
                     {name}
-                    <Button variant="destructive">Delete</Button>
+                    <Button variant="destructive" className="cursor-pointer" onClick={(e) => { e.preventDefault(); e.stopPropagation(); handleDelete(id); }}>
+                        Delete
+                    </Button>
                 </Card>
             </Link>
         </>
