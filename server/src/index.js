c3379d73f2943b12af986c0d5c72d4779db57b42
rudraa19 <rudra19206@gmail.com>
2025-09-10 22:04:32 +0530

    fix: listen vercel issue

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,26 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
 import V1Router from "../routes/v1/index.js";
 import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.status(200).send("server is running"));
-
 app.use("/v1", V1Router);
 
-async function main() {
-    await mongoose.connect(MONGO_URL);
-    app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+
+mongoose.connect(MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 }
 
-main();
+export default app;
